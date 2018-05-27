7fb57c6aac672ebd60345a27db577f8a7918e25d
eclipticrick <eclipticrick@gmail.com>
2018-05-27 23:13:45 +0200

    added a failing unit test

---
+++ src/test/java/nl/ecliptic/bep/friendspammer/Testing.java
@@ -0,0 +1,10 @@
+package nl.ecliptic.bep.friendspammer;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Testing {
+    @Test public void test_fail() {
+        fail(":)");
+    }
+}
